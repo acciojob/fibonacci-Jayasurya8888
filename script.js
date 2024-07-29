53a1a538321323436a0578559cfdbade81dffab6
Jayasurya8888 <jayasuryatummala7320@gmail.com>
2024-07-29 21:00:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function fibonacci(num) {
+	if (num === 0) return 0;
+    if (num === 1) return 0;
+    if (num === 2) return 1;
 	var res = (fibonacci(num-1)+fibonacci(num-2));
 	return res;
 // your code here
